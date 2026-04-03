7fdc024aa1878e3f6d8daaa54c17c2d0687bfe73
dabitdev <imdavidfernandez@gmail.com>
2026-04-03 02:16:19 -0400

    fix device-info problem

--- src/device-info.ts
+++ src/device-info.ts
@@ -2,20 +2,6 @@ import DeviceInfo from 'react-native-device-info'
 
 export type Platform = 'web' | 'react-native' | 'unknown'
 
-function detectPlatform(): Platform {
-    if (typeof window !== 'undefined' && typeof document !== 'undefined') {
-        return 'web'
-    }
-    if (
-        typeof navigator !== 'undefined' &&
-        navigator.product === 'ReactNative'
-    ) {
-        return 'react-native'
-    }
-
-    return 'unknown'
-}
-
 export async function getUserAgent() {
     return DeviceInfo.getUserAgent()
 }
@@ -25,14 +11,5 @@ export async function getDeviceId(): Promise<string> {
 }
 
 export async function getBundleId() {
-    const platform = detectPlatform()
-
-    switch (platform) {
-        case 'web':
-            return location.origin
-        case 'react-native':
-            return DeviceInfo.getBundleId()
-        default:
-            return 'Unknown'
-    }
+    return DeviceInfo.getBundleId()
 }
